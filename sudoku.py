2d7d91bfefdb8b645555f11da31dff331b50b031
chomter <chomter@gmail.com>
2016-04-21 22:21:01 +0200

    Update sudoku.py
    python file for C directory

--- sudoku.py
+++ sudoku.py
@@ -96,7 +96,7 @@ class Sudoku:
             return False  # Failed earlier
         if all(len(values[s]) == 1 for s in squares):
             return values  # Solved!
-        ## Chose the unfilled square s with the fewest possibilities
+        ## Chose the unfilled square s  with the fewest possibilities
         n, s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
         return self.some(self.search(self.assign(values.copy(), s, d)) for d in values[s])
 
